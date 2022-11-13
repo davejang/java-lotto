fb5261952852bec875868c2aff2329fa204f4e14
davejang <davejang98@naver.com>
2022-11-13 12:10:53 +0900

    feat: error message class
    에러 메세지 클래스 구현

--- src/main/java/lotto/utils/InputValidate.java
+++ src/main/java/lotto/utils/InputValidate.java
@@ -1,6 +1,10 @@
 package lotto.utils;
 
 import java.util.regex.Pattern;
+import lotto.utils.SystemMessage;
+
+import static lotto.utils.SystemMessage.ILLEGAL_INPUT_MONEY_DIGIT;
+import static lotto.utils.SystemMessage.ILLEGAL_INPUT_MONEY_UNIT;
 
 public class InputValidate {
     private static final String DIGIT_REGEX = "\\d+";
@@ -12,13 +16,13 @@ public class InputValidate {
 
     public static void isDigit(String money) {
         if(Pattern.matches(DIGIT_REGEX,money) == false) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ILLEGAL_INPUT_MONEY_DIGIT);
         }
     }
 
     public static void isThousandUnit(int money) {
         if(money % 1000 != 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ILLEGAL_INPUT_MONEY_UNIT);
         }
     }
 
--- src/main/java/lotto/utils/SystemMessage.java
+++ src/main/java/lotto/utils/SystemMessage.java
@@ -1,5 +1,6 @@
 package lotto.utils;
 
 public class SystemMessage {
-
+    public static final String ILLEGAL_INPUT_MONEY_DIGIT = "[ERROR] 금액은 숫자여야 합니다.";
+    public static final String ILLEGAL_INPUT_MONEY_UNIT = "[ERROR] 금액은 1000단위여야 합니다.";
 }
