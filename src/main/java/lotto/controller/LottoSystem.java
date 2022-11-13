d6e044d3965772c666d34de0f9fdf0ce726df5f0
davejang <davejang98@naver.com>
2022-11-13 12:16:02 +0900

    feat: lotto system controller
    로또 시스템 컨트롤러 구현

---
+++ src/main/java/lotto/controller/LottoSystem.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+
+public class LottoSystem {
+    public LottoSystem() {
+        InputView inputView = new InputView();
+        inputView.inputMoney();
+    }
+
+}
